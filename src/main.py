f087946265efa145f858567063cbf53452e87195
Chih-Wei Wu <chih-wei@lawn-143-215-82-144.lawn.gatech.edu>
2017-03-01 14:45:16 -0500

    five genres downloaded

--- src/main.py
+++ src/main.py
@@ -10,21 +10,21 @@ import retrieveFiles as rf
 #==== Enter valid chart names such as:
 # rock-songs; alternative-songs; pop-songs; adult-pop-songs; r-b-hip-hop-songs;
 # r-and-b-songs; latin-songs; latin-pop-songs; dance-electronic-songs; dance-club-play-songs
-chartName = 'latin-songs'
+chartName = 'hot-mainstream-rock-tracks'
 
 #==== Retrieve the song list
-# Retriever = rl.BillboardRetriever(chartName, '2017-01-24', 1, 2000)
-# List = Retriever.getList()
-# Retriever.writeList2txt()
-# Retriever.displayList()
+Retriever = rl.BillboardRetriever(chartName, '2017-01-24', 1, 2000)
+List = Retriever.getList()
+Retriever.writeList2txt()
+Retriever.displayList()
 
 #==== Retrieve the youtube links
 listpath = '../lists/' + chartName + '_2017-01-24.txt'
 Downloader = rf.YoutubeDownloader(chartName, listpath)
-# links = Downloader.getYoutubeLinks()
-# Downloader.displayLinks()
-# Downloader.writeLinks2txt()
-# print 'The final length of the youtube links %d' % len(links)
+links = Downloader.getYoutubeLinks()
+Downloader.displayLinks()
+Downloader.writeLinks2txt()
+print 'The final length of the youtube links %d' % len(links)
 
 #==== Download the files into the folder named after the Chart
 linkpath = '../links/' + chartName + '_links.txt'
